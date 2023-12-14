9b657f8c413d23bb43939100848fe79164142246
CookieDragon16 <93400671+CookieDragon16@users.noreply.github.com>
2023-12-14 18:27:11 +0200

    remove cout from ALU

--- ALU.vhd
+++ ALU.vhd
@@ -2,34 +2,28 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.NUMERIC_STD.all;
------------------------------------------------
----------- ALU 8-bit VHDL ---------------------
------------------------------------------------
+
 entity ALU is
     Port (
     a, b     : in  STD_LOGIC_VECTOR(7 downto 0);  -- 2 inputs 8-bit
-    op  : in  STD_LOGIC_VECTOR(3 downto 0);  -- 1 input 4-bit for selecting function
+    op  : in  STD_LOGIC_VECTOR(1 downto 0);  -- 1 input 4-bit for selecting function
     result   : out  STD_LOGIC_VECTOR(7 downto 0); -- 1 output 8-bit 
-    cout : out std_logic        -- Carryout flag
     );
 end ALU; 
 architecture Behavioral of ALU is
 
-signal ALU_Result : std_logic_vector (7 downto 0);
-signal tmp: std_logic_vector (8 downto 0);
+signal tmp : std_logic_vector (7 downto 0);
 
 begin
    process(a,b,op)
  begin
   case(op) is
   when "01" => -- Addition
-   ALU_Result <= a + b ; 
+   tmp <= a + b ; 
   when "10" => -- Subtraction
-   ALU_Result <= a - b ;
-  when others => ALU_Result <= a + b ; 
+   tmp <= a - b ;
+  when others =>
   end case;
  end process;
- result <= ALU_Result; -- ALU out
- tmp <= ('0' & a) + ('0' & b);
- cout <= tmp(8); -- Carryout flag
+ result <= tmp; -- ALU out
 end Behavioral;
